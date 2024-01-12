79dadac2c5b068b9d14c3f7dc1dfe715d419c631
Kaelan Smith <kaelancsmith@gmail.com>
2024-01-12 14:40:11 -0800

    add new layer utilities to twMerge plugin

--- src/twMergePlugin.ts
+++ src/twMergePlugin.ts
@@ -3,7 +3,10 @@ import { type Config, mergeConfigs, validators } from "tailwind-merge";
 type GroupIds =
   | "extendedShadows.offset-x"
   | "extendedShadows.offset-y"
-  | "extendedShadows.spread";
+  | "extendedShadows.spread"
+  | "extendedShadows.shadows"
+  | "extendedShadows.shadows-scale"
+  | "extendedShadows.shadows-ease";
 
 export const withExtendedShadows = (
   prevConfig: Config<GroupIds, string>
@@ -14,19 +17,13 @@ export const withExtendedShadows = (
         // x-axis shadow offsets
         "extendedShadows.offset-x": [
           {
-            "shadow-l": ["", validators.isNumber, validators.isArbitraryLength],
-          },
-          {
-            "shadow-r": ["", validators.isNumber, validators.isArbitraryLength],
+            "shadow-x": ["", validators.isNumber, validators.isArbitraryLength],
           },
         ],
         // y-axis shadow offsets
         "extendedShadows.offset-y": [
           {
-            "shadow-t": ["", validators.isNumber, validators.isArbitraryLength],
-          },
-          {
-            "shadow-b": ["", validators.isNumber, validators.isArbitraryLength],
+            "shadow-y": ["", validators.isNumber, validators.isArbitraryLength],
           },
         ],
         // shadow spread
@@ -37,6 +34,24 @@ export const withExtendedShadows = (
               validators.isNumber,
               validators.isArbitraryLength,
             ],
+          },
+        ],
+        // shadows (layers)
+        "extendedShadows.shadows": [
+          {
+            shadows: [validators.isInteger],
+          },
+        ],
+        // shadows scale multiplier
+        "extendedShadows.shadows-scale": [
+          {
+            "shadows-scale": [validators.isNumber],
+          },
+        ],
+        // shadows easings
+        "extendedShadows.shadows-ease": [
+          {
+            "shadows-ease": ["in", "out"],
           },
         ],
       },
